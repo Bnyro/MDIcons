dac9df5533e7df6d7021e72ff82569b624eb01f6
Bnyro <bnyro@tutanota.com>
2023-02-27 17:18:58 +0100

    Support usage with search queries

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Icons from '../../components/Icons.svelte';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	let query;
+
+	onMount(() => {
+		const params = new URLSearchParams(location.search);
+		query = params.get('q');
+	});
+</script>
+
+<Icons searchQuery={query} />
